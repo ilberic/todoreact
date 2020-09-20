d42eba608bc722618a91b82c01695ec978630175
Vadim <cltcmdc2001@gmail.com>
2020-09-20 20:43:48 +0500

    Updates

---
+++ precache-manifest.373347a6d5e9796754ebd7dac713c64b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb88089f9ce6bfaba5ae23d12d72160",
+    "url": "/todoreact/index.html"
+  },
+  {
+    "revision": "756440c8b8a1d63380d2",
+    "url": "/todoreact/static/css/main.da9df54e.chunk.css"
+  },
+  {
+    "revision": "72062523a56d90604482",
+    "url": "/todoreact/static/js/2.f12c974f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todoreact/static/js/2.f12c974f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "756440c8b8a1d63380d2",
+    "url": "/todoreact/static/js/main.3107601c.chunk.js"
+  },
+  {
+    "revision": "48ee062f590307f45809",
+    "url": "/todoreact/static/js/runtime-main.0f33e588.js"
+  }
+]);
